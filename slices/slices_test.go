6d17b3c37bec5bba1947bd0e4516e4f6d6de73d7
Bartlomiej Szostek <szostek.bartek@gmail.com>
2018-10-21 00:20:31 +0100

    Sum accepts a slice rather than fixed size array

--- slices/slices_test.go
+++ slices/slices_test.go
@@ -6,16 +6,25 @@ import (
 )
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
-	if want != got {
-		t.Errorf("got '%d', want '%d', given %v", got, want, numbers)
+
+	assertCollectionSum := func(got int, want int, slice []int, t *testing.T) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%d', want '%d', given %v", got, want, slice)
+		}
 	}
+
+	t.Run("Collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
+		assertCollectionSum(got, want, numbers, t)
+	})
+
 }
 
 func ExampleSum() {
-	numbers := [5]int{1, 1, 1, 1, 1}
+	numbers := []int{1, 1, 1, 1, 1}
 	sum := Sum(numbers)
 	fmt.Println(sum)
 	// Output: 5
