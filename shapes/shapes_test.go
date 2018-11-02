cc379caba0ef313d22436eb50f363f0ab9805d48
Bartlomiej Szostek <szostek.bartek@gmail.com>
2018-11-02 14:46:49 +0000

    Added Shape interface with Area method. Refactored tests using array test schema.

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -15,15 +15,25 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
-	if got != want {
-		t.Errorf("got %.2f, want %.2f", got, want)
+	areaTests := []struct {
+		shape Shape
+		want float64
+	}{
+		{Rectangle{12.0, 6.0}, 72.0},
+		{Circle{10.0}, 314.1592653589793},
+	}
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f, want %.2f", got, tt.want)
+		}
 	}
 }
 
 func ExamplePerimeter() {
-	fmt.Println("")
+	rectangle := Rectangle{1.0, 2.0}
+	perimeter := Perimeter(rectangle)
+	fmt.Println(perimeter)
+	// Output: 6
 }
 
