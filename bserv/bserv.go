5224aa3dd47a134cff94ad939b6a23c92a072cec
Bartlomiej Szostek <szostek.bartek@gmail.com>
2018-11-11 19:01:25 +0000

    Introduced PlayerStore interface with default in-memory implementation. Unit tests improvements

--- bserv/bserv.go
+++ bserv/bserv.go
@@ -5,7 +5,37 @@ import (
 	"net/http"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+type PlayerStore interface {
+	GetPlayerScore(name string) int
 }
 
+type VolatilePlayerStore struct {
+}
+
+func (v *VolatilePlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
+	score := p.store.GetPlayerScore(player)
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+	fmt.Fprint(w, score)
+}
+
+func GetPlayerScore(name string) int {
+	switch name {
+	case "Floyd":
+		return 10
+	case "Pepper":
+		return 20
+	default:
+		return 0
+	}
+}
