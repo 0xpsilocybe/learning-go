51043bccb42efa0d3988bb43b68f0825c4667e76
Bartlomiej Szostek <bartek.szostek@limejump.com>
2018-12-26 01:06:49 +0000

    Feat: Scheduling correct blinds at correct times

--- bserv/CLI_test.go
+++ bserv/CLI_test.go
@@ -1,8 +1,10 @@
 package poker_test
 
 import (
+	"fmt"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/0xpsilocybe/learning-go/bserv"
 )
@@ -28,14 +30,46 @@ func TestCLI(t *testing.T) {
 	})
 
 	t.Run("it schedules printing blind values", func(t *testing.T) {
+		// Arrange
 		in := strings.NewReader("Bob wins\n")
 		playerStore := &poker.StubPlayerStore{}
 		blindAlerter := &poker.SpyBlindAlerter{}
 		cli := poker.NewCLI(playerStore, in, blindAlerter)
+		// Act
 		cli.PlayPoker()
-		if len(blindAlerter.Alerts) != 1 {
-			t.Fatal("expected a blind alert to be scheduled")
+		cases := []poker.ScheduledAlert{
+			{0 * time.Second, 100},
+			{10 * time.Minute, 200},
+			{20 * time.Minute, 300},
+			{30 * time.Minute, 400},
+			{40 * time.Minute, 500},
+			{50 * time.Minute, 600},
+			{60 * time.Minute, 800},
+			{70 * time.Minute, 1000},
+			{80 * time.Minute, 2000},
+			{90 * time.Minute, 4000},
+			{100 * time.Minute, 8000},
+		}
+		for i, want := range cases {
+			t.Run(fmt.Sprintf("%d scheduled for %v", want.Amount, want.At), func(t *testing.T) {
+				// Assert
+				if len(blindAlerter.Alerts) <= i {
+					t.Fatalf("alert %d was not scheduled %v", i, blindAlerter.Alerts)
+				}
+				got := blindAlerter.Alerts[i]
+				assertScheduledAlert(t, got, want)
+			})
 		}
 	})
 
+}
+
+func assertScheduledAlert(t *testing.T, got, want poker.ScheduledAlert) {
+	t.Helper()
+	if got.Amount != want.Amount {
+		t.Errorf("got amount %d, want %d", got.Amount, want.Amount)
+	}
+	if got.At != want.At {
+		t.Errorf("got scheduled time of %v, want %v", got.At, want.At)
+	}
 }
--- bserv/testing.go
+++ bserv/testing.go
@@ -40,18 +40,21 @@ func AssertPlayerWin(t *testing.T, store *StubPlayerStore, winner string) {
 	}
 }
 
+type ScheduledAlert struct {
+	At     time.Duration
+	Amount int
+}
+
+func (s ScheduledAlert) String() string {
+	return fmt.Sprintf("%d chips at %v", s.Amount, s.At)
+}
+
 type SpyBlindAlerter struct {
-	Alerts []struct {
-		scheduledAt time.Duration
-		amount      int
-	}
+	Alerts []ScheduledAlert
 }
 
 func (s *SpyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
-	s.Alerts = append(s.Alerts, struct {
-		scheduledAt time.Duration
-		amount      int
-	}{duration, amount})
+	s.Alerts = append(s.Alerts, ScheduledAlert{duration, amount})
 }
 
 func NewGetScoreRequest(name string) *http.Request {
